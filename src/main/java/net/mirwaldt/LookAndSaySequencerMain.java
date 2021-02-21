c08067b530ede151c22ebcc80c9dad14343fca6a
mmirwaldt <michael.mirwaldt@gmail.com>
2021-02-22 00:17:08 +0100

    part one and part two solved

---
+++ src/main/java/net/mirwaldt/LookAndSaySequencerMain.java
@@ -0,0 +1,15 @@
+package net.mirwaldt;
+
+public class LookAndSaySequencerMain {
+    public static void main(String[] args) {
+        final LookAndSaySequencer lookAndSaySequencer = new DefaultLookAndSaySequencer();
+
+        // Part one:
+        // result - 492982
+        System.out.println(lookAndSaySequencer.sequenceLookAndSay("1321131112", 40).length());
+
+        // Part two:
+        // result - 6989950
+        System.out.println(lookAndSaySequencer.sequenceLookAndSay("1321131112", 50).length());
+    }
+}
